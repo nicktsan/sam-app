c54e2b6045d7c210745de79c2d09885bf8433a52
nicktsan <nicktsang.manhay@gmail.com>
2023-02-26 20:13:37 -0800

    Changed return message

--- hello_world/app.py
+++ hello_world/app.py
@@ -32,11 +32,9 @@ def lambda_handler(event, context):
     #     print(e)
 
     #     raise e
-
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "hello my friend",
-            # "location": ip.text.replace("\n", "")
+            "message": "I'm using canary deployments",
         }),
     }
